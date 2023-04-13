7539956159c6fffd6ecbbec08b08e8037089355d
mepix <mepix@MohammadHP>
2023-04-14 03:46:48 +0400

    Finished Person class Added Copy and Def cons to Date class

--- src/jail/Date.java
+++ src/jail/Date.java
@@ -4,11 +4,23 @@ public class Date {
     private int day;
     private int month;
     private int year;
-
+    
+    public Date(){
+        this.day = 0;
+        this.month = 0;
+        this.year = 0;
+    }
+    
     public Date(int day, int month, int year) {
         this.day = day;
         this.month = month;
         this.year = year;
+    }
+    
+    public Date(Date d1) {
+        this.day = d1.day;
+        this.month = d1.month;
+        this.year = d1.year;
     }
 
     public int getDay() {
