f3588b09050db5a03553661d5d19836edb717dca
AzizKri <aziz.kanakri@gmail.com>
2023-04-15 02:59:10 +0400

    Fix toString

--- src/jail/Visitation.java
+++ src/jail/Visitation.java
@@ -100,6 +100,6 @@ public class Visitation {
 
     @Override
     public String toString() {
-        return "VISIT:" + visitationID + "\nVisitors: " + visitors + "\nPrisoners: " + prisoners + "\nDate of Visit: " + dateOfVisit + "\nTime: " + time + "\n";
+        return "VISIT " + visitationID + "\nVisitors: " + visitors + "\nPrisoners: " + prisoners + "\nDate of Visit: " + dateOfVisit + "\nTime: " + time + "\n";
     }
 }
