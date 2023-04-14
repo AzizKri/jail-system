2478aa087be49953e4a2e88c0d8daa8f96bc7080
Ayman Bashir <124577066+Aym2nn@users.noreply.github.com>
2023-04-14 08:00:34 +0400

    done with crime class

--- src/jail/Crime.java
+++ src/jail/Crime.java
@@ -1,1 +1,41 @@
+package jail;
+
+public class Crime{
+    private String offense;
+    private int duration;
+    
+    public Crime(){
+        this(null,0);
+    }
+    
+    public Crime(String offense, int duration){
+        this.offense = offense;
+        this.duration = duration;
+    }
+
+    public String getOffense() {
+        return offense;
+    }
+
+    public void setOffense(String offense) {
+        this.offense = offense;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Crime Details: "
+                + "\nOffense: " + offense + 
+                "\nDuration: "+duration;
+    }
+    
+    
+}
 
