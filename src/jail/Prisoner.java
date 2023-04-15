9a55d4a22935bea262fbf48bfccaf27f47bf6929
DELL <DELL@DESKTOP-N9TOLK4>
2023-04-15 11:14:40 +0400

    The class is not done yet. The constructors are work in progress.

--- src/jail/Prisoner.java
+++ src/jail/Prisoner.java
@@ -1,6 +1,73 @@
 package jail;
 
 
-public class Prisoner {
+public class Prisoner extends Person{
+    private String offense;
+    private int duration;
+    private Date entry;
+    private String inmateID;
+
+    public Prisoner() {
+    }
+
+    public Prisoner(String a, int b, char c, String d, String e, int f, int g, int h, int i){
+        
+    }
+    
+    public Prisoner(String a, int b, char c, String d, String e, int f, Date g){
+        
+    }
+    
+    public Prisoner(Person p, String str, int i, Date d){
+        
+    }
+    
+    public Prisoner(Prisoner copy){
+        this.offense = copy.offense;
+        this.duration = copy.duration;
+        this.entry = new Date(copy.entry);
+        this.inmateID = copy.inmateID + "COPY";
+    }
+    
+    public void setOffense(String offense) {
+        this.offense = offense;
+    }
+    
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public void setEntry(Date entry) {
+        this.entry = entry;
+    }
+
+    public void setInmateID(String inmateID) {
+        this.inmateID = inmateID;
+    }
+    
+    public String getOffense() {
+        return offense;
+    }
+    
+    public int getDuration() {
+        return duration;
+    }
+
+    public Date getEntry() {
+        return entry;
+    }
+
+    public String getInmateID() {
+        return inmateID;
+    }
+    
+    @Override
+    public String toString(){
+        return "";
+    }
+
+    
+    
     
 }
+
