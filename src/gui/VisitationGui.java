061b2fa3fc2e07414515ea398ac849ede4384fa1
AzizKri <aziz.kanakri@gmail.com>
2023-04-20 02:44:20 +0400

    Remove unnecessary comment

--- src/gui/VisitationGui.java
+++ src/gui/VisitationGui.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JInternalFrame.java to edit this template
- */
 package gui;
 
 import jail.Database;
@@ -11,10 +7,6 @@ import jail.Date;
 import jail.Visitation;
 import java.util.ArrayList;
 
-/**
- *
- * @author husse
- */
 public class VisitationGui extends javax.swing.JInternalFrame {
 
     /**
